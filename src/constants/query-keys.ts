93d5376c4403d2e01481e01cd63739d949de84ed
nacasha <izalfathoni@gmail.com>
2021-05-24 18:41:34 +0700

    Add more query keys constants

--- src/constants/query-keys.ts
+++ src/constants/query-keys.ts
@@ -1,4 +1,11 @@
-export const ASSIGNMENTS = 'assignments';
-export const EMPLOYEES = 'employees';
-export const QUESTIONS = 'questions';
-export const REVIEWS = 'reviews';
+export const FETCH_ASSIGNMENT = 'assignments';
+export const GET_ASSIGNMENT = 'assignment';
+
+export const FETCH_EMPLOYEE = 'employees';
+export const GET_EMPLOYEE = 'employee';
+
+export const FETCH_QUESTION = 'questions';
+export const GET_QUESTION = 'question';
+
+export const FETCH_REVIEW = 'reviews';
+export const GET_REVIEW = 'review';
