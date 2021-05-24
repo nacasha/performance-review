dfd9dbd21b24e60a2022c8a900b2284ca8f2807b
nacasha <izalfathoni@gmail.com>
2021-05-24 18:57:56 +0700

    Add breadcrumb to not found page template

--- src/ui/templates/not-found-page-template/not-found-page-template.tsx
+++ src/ui/templates/not-found-page-template/not-found-page-template.tsx
@@ -1,18 +1,25 @@
 import { FC } from 'react';
 import { Helmet } from 'react-helmet-async';
 import { Text } from '@fluentui/react';
+import { useBreadcrumb } from '@nacasha/fluentui-kit.ui.navbar';
 
 import { PageContainer } from 'src/ui/elements/page-container';
 
-export const NotFoundPageTemplate: FC = () => (
-  <PageContainer>
-    <Helmet>
-      <title>Not Found</title>
-    </Helmet>
+export const NotFoundPageTemplate: FC = () => {
+  useBreadcrumb([
+    { key: 'not-found', text: 'Not Found', isCurrentItem: true },
+  ]);
 
-    <Text variant="large">
-      <strong>404 Not Found</strong>
-    </Text>
-    <Text>We Cannot Find the Data You&rsquo;re Searching For</Text>
-  </PageContainer>
-);
+  return (
+    <PageContainer>
+      <Helmet>
+        <title>Not Found</title>
+      </Helmet>
+
+      <Text variant="large">
+        <strong>404 Not Found</strong>
+      </Text>
+      <Text>We Cannot Find the Data You&rsquo;re Searching For</Text>
+    </PageContainer>
+  );
+};
