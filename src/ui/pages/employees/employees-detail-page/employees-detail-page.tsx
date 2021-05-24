1b62f32ec149e74e7226d42a1a8efcc83466f74a
nacasha <izalfathoni@gmail.com>
2021-05-24 18:58:22 +0700

    Refactor employees detail page

--- src/ui/pages/employees/employees-detail-page/employees-detail-page.tsx
+++ src/ui/pages/employees/employees-detail-page/employees-detail-page.tsx
@@ -1,39 +1,24 @@
 import { Card } from '@nacasha/fluentui-kit.ui.card';
 import { FC } from 'react';
-import { Helmet } from 'react-helmet-async';
-import axios from 'axios';
 
+import { DetailPageTemplate } from 'src/ui/templates/detail-page-template';
 import { employeeApi } from 'src/services/api';
-import { NotFoundPageTemplate } from 'src/ui/templates/not-found-page-template';
-import { PageContainer } from 'src/ui/elements/page-container';
-import { PageSpinner } from 'src/ui/elements/page-spinner';
 import { QueryKeys } from 'src/constants';
 import { useQueryGetByID } from 'src/hooks/query-hooks';
 
 export const EmployeesDetailPage: FC = () => {
   const { error, isFetching } = useQueryGetByID(QueryKeys.GET_EMPLOYEE, employeeApi.get);
 
-  if (isFetching) {
-    return <PageSpinner />;
-  }
-
-  if (error) {
-    if (axios.isAxiosError(error)) {
-      if (error.response?.status === 404) {
-        return <NotFoundPageTemplate />;
-      }
-    }
-  }
-
   return (
-    <PageContainer centered>
-      <Helmet>
-        <title>Izal Fathoni | Employees</title>
-      </Helmet>
-
+    <DetailPageTemplate
+      title="Detail Employee"
+      breadcrumb={['Employees', 'Detail', 'Izal Fathoni']}
+      error={error}
+      loading={isFetching}
+    >
       <Card cardTitle="Details">
         Employee Detail
       </Card>
-    </PageContainer>
+    </DetailPageTemplate>
   );
 };
