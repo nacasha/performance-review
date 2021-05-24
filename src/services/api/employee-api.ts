a22c2dcf230362891360e65c7922b17cb423cb4e
nacasha <izalfathoni@gmail.com>
2021-05-24 18:41:59 +0700

    Add employee api

--- src/services/api/employee-api.ts
+++ src/services/api/employee-api.ts
@@ -1,4 +1,21 @@
 import { axiosApiClient } from 'src/lib/axios/axios-api-client';
+import { IEmployee } from 'src/typings';
 import { IFetchParams, IFetchResponse } from 'src/typings/api';
 
-export const fetch = (params: IFetchParams) => axiosApiClient.get<IFetchResponse>('employees', { params });
+export type IEmployeeFetchRequest = IFetchParams;
+export type IEmployeeFetchResponse = IFetchResponse<IEmployee>;
+
+export type IEmployeeAddRequest = Record<string, unknown>;
+export type IEmployeeAddResponse = { data: any };
+
+export const fetch = (params: IEmployeeFetchRequest) => (
+  axiosApiClient.get<IEmployeeFetchResponse>('employees', { params })
+);
+
+export const get = (id: any) => (
+  axiosApiClient.get<IEmployee>(`employees/${id}`)
+);
+
+export const add = (data: IEmployeeAddRequest) => (
+  axiosApiClient.post<IEmployeeAddResponse>('employees', data)
+);
